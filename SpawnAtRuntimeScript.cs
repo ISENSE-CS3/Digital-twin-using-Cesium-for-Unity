4214fd9ea6d11353afee6a36568c7085188b9649
Abigail Joseph <jemimahjosep2021@fau.edu>
2023-11-15 11:05:04 -0500

    add files

---
+++ SpawnAtRuntimeScript.cs
@@ -0,0 +1,21 @@
+/ This script uses the Instantiate function to spawn a Gameobject when the user enters Play mode.
+// Use this script to spawn agents when play mode is entered.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAtRuntime : MonoBehaviour
+{
+    public GameObject spawnobject;
+
+    void Start()
+    {
+        Instantiate(spawnobject);
+    }
+
+    void Update()
+    {
+
+    }
+}
